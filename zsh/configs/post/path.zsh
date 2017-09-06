1591ee102b9a66635426aeef10aaf4daf0b5d7c8
Ranbir Singh <ransingh57@yahoo.com>
2017-09-06 10:27:11 +1000

    updated rspec and wpathg

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,5 +1,7 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+PATH="$HOME/.rbenv/bin:$PATH"
+PATH="$PYENV_ROOT/bin:$PATH"
 
 # load rbenv if available
 if command -v rbenv >/dev/null; then
